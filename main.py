594579b46d86d621e577cd4183906d4ef49b56d0
Thomas Kreeger <takreeger@mail.com>
2019-06-05 16:32:53 -0700

    Add support for loading index.html template

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
-    def get(self): #for a get request
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+    def get(self):
+        self.response.headers["Content-Type"] = "text/html"
+        index_template = the_jinja_env.get_template("templates/index.html")
+        self.response.write(index_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
