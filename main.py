916fcd3e353b270d0557219b09ca6c13c4ef8124
Sam Sweeney <sam.s.sweeney@gmail.com>
2019-06-05 17:03:09 -0700

    Add more quotes and add JS

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ the_jinja_env = jinja2.Environment(
 QUOTES = [
     "Smile, it looks good on you.",
     "Find what makes you happy, and do more of that.",
+    "You matter.",
+    "Be a voice not an echo.",
+    "You are the only one who can limit your greatness.",
+    "Make the most of yourself... for that is all there is of you.",
+    "It takes courage to grow up and become who you really are.",
+    "The two most important days in your life are the day you are born and the day you find out why.",
+    "It is never too late to be what you might have been.",
+    "Whatever you are, be a good one.",
+    "The true success is the person who invented himself.",
+    "Give up on being perfect and start working on becoming yourself.",
+    "Do something today that your future self will thank you for.",
+    "Whatever you do, do with all your might.",
+    "You were put on this Earth to achieve your greatest self, to live out your purpose, and to do it courageously.",
+    "Definiteness of purpose is the starting point of all achievement.",
+    "All our dreams can come true, if we have the courage to pursue them.",
+    "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine.",
+    "Opportunities don't happen. You create them.",
+    "Excuses will always be there for you. Opportunity won't.",
 ]
 
 def get_quote():
